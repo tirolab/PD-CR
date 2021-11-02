d437e57de0e67935a7f30a5f87de4d12c343a5c9
tirolab <69150276+tirolab@users.noreply.github.com>
2021-11-02 11:06:01 +0100

    Update Script_rhoComputing.py

--- Script_rhoComputing.py
+++ Script_rhoComputing.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
-# Written by i3s and the TIRO-lab
+# Written by i3s and the TIRO-lab - UCA, Nice, FRANCE (D.CHARDIN, M.BARLAUD)
 # Last update : O5 august 2020
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import functions as ff
 import numpy as np
